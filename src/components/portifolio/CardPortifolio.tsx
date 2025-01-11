af98008822b58f638aba618ac105db87b9f7f8fe
Thiago Lago <thiagolagoo2@gmail.com>
2025-01-11 12:36:20 -0300

    minor redirect fix

--- src/components/portifolio/CardPortifolio.tsx
+++ src/components/portifolio/CardPortifolio.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface PortifolioObject {
   id: string;
@@ -20,6 +20,8 @@ export default function CardPortifolioComponent(
   {
     portifolioObject
   }: Readonly<CardPortifolioComponentProps>) {
+
+  const router = useRouter();
 
   return (
     <div className="flex flex-col mdd:flex-row justify-between text-white pt-6 pb-6 pl-0 pr-6 md:pt-12 md:pb-12 md:pr-12 md:pl-0">
@@ -43,7 +45,6 @@ export default function CardPortifolioComponent(
       </div>
 
       <div className="flex items-start justify-center cursor-pointer my-6 md:my-0 md:mx-12 transition-transform duration-300 hover:scale-105">
-        <Link href={process.env.NEXT_PUBLIC_APP_ENV || process.env.NEXT_PUBLIC_APP_ENV === 'dev' ? `/${portifolioObject.route}` : `meu-portfolio/${portifolioObject.route}` }>
           <Image
             src={portifolioObject.image}
             width={391}
@@ -52,8 +53,8 @@ export default function CardPortifolioComponent(
             title={portifolioObject.title}
             priority
             className="rounded-lg w-full max-w-xs md:max-w-md"
+            onClick={() => router.push(`${portifolioObject.route}`)}
           />
-        </Link>
       </div>
 
       <div className="flex flex-col space-y-4 mdd:max-w-lg justify-center md:mt-4">
@@ -63,7 +64,7 @@ export default function CardPortifolioComponent(
         <p className="text-[14px] text-white">
           {portifolioObject.text}
         </p>
-        <a href={`/${portifolioObject.route}`} className="flex items-center text-sm font-semibold text-white underline hover:text-gray-400 justify-end">
+        <a href={process.env.NEXT_PUBLIC_APP_ENV || process.env.NEXT_PUBLIC_APP_ENV === 'dev' ? `/${portifolioObject.route}` : `meu-portfolio/${portifolioObject.route}` } className="flex items-center text-sm font-semibold text-white underline hover:text-gray-400 justify-end">
           Saiba mais
           <Image
             src={`images/arrow_outward-2.svg`}
